2c829f8933bedc060f49edc23bfe7873b6d1f63c
Aditya Revankar <adityarevankar03@gmail.com>
2025-02-26 03:10:30 +0530

    bugs fixedd

--- react-vite-shadcn-ui/src/App.tsx
+++ react-vite-shadcn-ui/src/App.tsx
@@ -1,6 +1,4 @@
-import CountBtn from "@/components/count-btn";
-import ReactSVG from "@/assets/react.svg";
-import { Badge } from "@/components/ui/badge";
+
 import EasyHealthLanding from "./components/EasyHealthLanding";
 function App() {
   return (
